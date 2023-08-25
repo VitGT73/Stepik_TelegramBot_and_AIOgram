aa5bda31c800fb0c5d2c9fe24fba82093511c23d
VitGT73 <tvit@mail.ru>
2023-08-25 18:59:58 +0500

    edit main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# 6344478735:AAFUq_h4bTqai5H4cGbXJ1iYC0wmTwlb7PA
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
