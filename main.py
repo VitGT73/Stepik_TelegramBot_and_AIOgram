ae5bf2957b4bfd430dda35c79c87aef9dad68059
VitGT73 <tvit@mail.ru>
2023-08-29 16:58:14 +0500

    Добаил файл для переменных окружения .env.example

--- main.py
+++ main.py
@@ -1,30 +1,4 @@
-import requests
-import json
-from pprint import pprint
-import config
-import time
+anonymous_filter = lambda x: x.lower().count("я") >= 23
 
 
-API_URL = 'https://api.telegram.org/bot'
-BOT_TOKEN = config.BOT_TOKEN
-TEXT = "И вам здрасте!"
-MAX_COUNTER = 100
-
-offset = -2
-counter = 0
-chat_id = 787486073
-
-while counter < MAX_COUNTER:
-
-    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
-
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
-
-    if updates['result']:
-        for result in updates['result']:
-            offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
-
-    time.sleep(1)
-    counter += 1
+print(anonymous_filter('яяяяяяяяяяяяяяяяяяяяяяяя, яяяяяяяяяяяяяяяя и яяяяяяяя тоже!'))
