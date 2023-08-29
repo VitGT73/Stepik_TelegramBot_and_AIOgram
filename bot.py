e9194e445eb6254d2a0b40e1b6a0510aa72934e7
VitGT73 <tvit@mail.ru>
2023-08-29 18:20:24 +0500

    Основным файлом сделал bot.py, а main.py оставил длятестирования коротких кусков кода.

---
+++ bot.py
@@ -0,0 +1,18 @@
+from config import Config, TgBot, DatabaseConfig
+import random
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
+from environs import Env
+
+
+def load_config(path: str | None) -> Config:
+    env: Env = Env()
+    env.read_env(path)
+
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN'),
+                               admin_ids=list(map(int, env.list('ADMIN_IDS')))),
+                  db=DatabaseConfig(database=env('DATABASE'),
+                                    db_host=env('DB_HOST'),
+                                    db_user=env('DB_USER'),
+                                    db_password=env('DB_PASSWORD')))
